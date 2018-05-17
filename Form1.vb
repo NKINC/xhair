b92a17a2741a72c2932ae283f7d455478b11bc45
NKINC <NKINC@users.noreply.github.com>
2018-05-17 07:10:52 -0400

    Add files via upload

--- Form1.vb
+++ Form1.vb
@@ -9,7 +9,7 @@ Public Class Form1
     Public lineWidth As Single = 1
     Public lineWidthArc As Single = 1
     Public timerInterval As Integer = 1
-    Public eclipseDiameter As Single = 4.5F
+    Public eclipseDiameter As Single = 6.0F
     Public eclipseBrushTransparency As Integer = 255 '255
     Public eclipseBrushColorOffset As Integer = 64
     Public eclipseDiameterQuantifier As Integer = 7
@@ -29,12 +29,13 @@ Public Class Form1
             Me.WindowState = FormWindowState.Maximized
             Me.DoubleBuffered = True
             Me.SetStyle(ControlStyles.OptimizedDoubleBuffer, True)
+            Me.SetStyle(ControlStyles.SupportsTransparentBackColor, True)
             Me.AutoSizeMode = AutoSizeMode.GrowOnly
             Return
         Catch ex As Exception
             Err.Clear()
         Finally
-            Me.Invalidate()
+            Me.Invalidate(True)
         End Try
     End Sub
     'Dim timeout As Integer = 10
@@ -55,10 +56,10 @@ Public Class Form1
                         g.CompositingMode = CompositingMode.SourceOver
 
                         ' GET CENTER PIXEL COLOR ON SCREEN
-                        Dim Bmp As New Bitmap(1, 1)
-                        Dim gTemp As Graphics = Graphics.FromImage(Bmp)
-                        gTemp.CopyFromScreen(New Point(CInt((Me.ClientRectangle.Width / 2) + offsetX - eclipseDiameter / 2), CInt((Me.ClientRectangle.Height / 2) + offsetY - eclipseDiameter / 2)), New Point(0, 0), Bmp.Size)
-                        Dim colorOfScreenPixel As Color = Color.FromArgb(255, Bmp.GetPixel(0, 0))
+                        'Dim Bmp As New Bitmap(1, 1)
+                        'Dim gTemp As Graphics = Graphics.FromImage(Bmp)
+                        'gTemp.CopyFromScreen(New Point(CInt((Me.ClientRectangle.Width / 2) + offsetX - eclipseDiameter / 2), CInt((Me.ClientRectangle.Height / 2) + offsetY - eclipseDiameter / 2)), New Point(0, 0), Bmp.Size)
+                        'Dim colorOfScreenPixel As Color = Color.FromArgb(255, Bmp.GetPixel(0, 0))
 
                         'Dim eclipseBrushColor As Color = baseColor1
                         ' DRAW SOLID CIRCLE IN CENTER OF SCREEN
@@ -72,7 +73,7 @@ Public Class Form1
                         eclipseBrush = New SolidBrush(baseColor1)
                         g.FillEllipse(eclipseBrush, CSng((Me.ClientRectangle.Width / 2) + offsetX - eclipseDiameter / 2), CSng((Me.ClientRectangle.Height / 2) + offsetY - eclipseDiameter / 2), eclipseDiameter, eclipseDiameter)
 
-                        eclipsePen.Width = 0.05F
+                        eclipsePen.Width = 1.0F
                         g.DrawEllipse(eclipsePen, CSng((Me.ClientRectangle.Width / 2) + offsetX - eclipseDiameter / 2), CSng((Me.ClientRectangle.Height / 2) + offsetY - eclipseDiameter / 2), eclipseDiameter, eclipseDiameter)
 
                         'OUTER ECLIPSE
@@ -110,15 +111,248 @@ Public Class Form1
         Finally
             Me.TopMost = True
             Me.BringToFront()
-            Me.Invalidate()
+            Me.Invalidate(True)
         End Try
+    End Sub
+    Private Sub Form1_LocationChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.LocationChanged
+        Me.Invalidate(True)
+    End Sub
+
+    Private Sub Form1_LostFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.LostFocus
+        Me.Invalidate(True)
     End Sub
     Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
         pauseDrawing = True
-        Me.Invalidate()
+        Me.Invalidate(True)
     End Sub
+
+    'Private Sub Timer1_Tick(sender As Object, e As EventArgs)
+    '    Try
+    '        ' NUM-KEY LOCK TOGGGLES CROSS HAIR DOT
+    '        'If True = True Then 'If timeOutTime.AddMilliseconds(timeout) <= DateTime.Now Then '
+    '        If pauseDrawing = False Or Control.IsKeyLocked(Keys.NumLock) = True Then
+    '            baseColor1 = Color.FromArgb(eclipseBrushTransparency, baseColor1)
+    '            Using g As Graphics = Graphics.FromHwnd(IntPtr.Zero)
+    '                Try
+    '                    ' SET IMAGE SETTINGS
+    '                    g.CompositingQuality = Drawing2D.CompositingQuality.AssumeLinear
+    '                    g.InterpolationMode = Drawing2D.InterpolationMode.HighQualityBicubic
+    '                    g.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+    '                    g.PixelOffsetMode = PixelOffsetMode.HighQuality
+    '                    g.CompositingMode = CompositingMode.SourceOver
+
+    '                    ' GET CENTER PIXEL COLOR ON SCREEN
+    '                    'Dim Bmp As New Bitmap(1, 1)
+    '                    'Dim gTemp As Graphics = Graphics.FromImage(Bmp)
+    '                    'gTemp.CopyFromScreen(New Point(CInt((Me.ClientRectangle.Width / 2) + offsetX - eclipseDiameter / 2), CInt((Me.ClientRectangle.Height / 2) + offsetY - eclipseDiameter / 2)), New Point(0, 0), Bmp.Size)
+    '                    'Dim colorOfScreenPixel As Color = Color.FromArgb(255, Bmp.GetPixel(0, 0))
+
+    '                    'Dim eclipseBrushColor As Color = baseColor1
+    '                    ' DRAW SOLID CIRCLE IN CENTER OF SCREEN
+    '                    'eclipseBrush = New SolidBrush(Color.FromArgb(CInt(255), Color.White))
+    '                    'Dim eclipseDiameter2 As Single = eclipseDiameter * 2
+    '                    'g.FillEllipse(eclipseBrush, CSng((Me.ClientRectangle.Width / 2) + offsetX - eclipseDiameter2 / 2), CSng((Me.ClientRectangle.Height / 2) + offsetY - eclipseDiameter2 / 2), eclipseDiameter2, eclipseDiameter2)
+
+    '                    'eclipseBrushColor = baseColor1
+    '                    ' DRAW SOLID CIRCLE IN CENTER OF SCREEN
+
+    '                    eclipseBrush = New SolidBrush(baseColor1)
+    '                    g.FillEllipse(eclipseBrush, CSng((Me.ClientRectangle.Width / 2) + offsetX - eclipseDiameter / 2), CSng((Me.ClientRectangle.Height / 2) + offsetY - eclipseDiameter / 2), eclipseDiameter, eclipseDiameter)
+
+    '                    eclipsePen.Width = 1.0F
+    '                    g.DrawEllipse(eclipsePen, CSng((Me.ClientRectangle.Width / 2) + offsetX - eclipseDiameter / 2), CSng((Me.ClientRectangle.Height / 2) + offsetY - eclipseDiameter / 2), eclipseDiameter, eclipseDiameter)
+
+    '                    'OUTER ECLIPSE
+    '                    'Try
+    '                    '    'CROSSHAIRS
+    '                    '    Dim pts As New List(Of PointF)
+    '                    '    '  VERTICAL CROSSHAIR LINE
+    '                    '    Dim verticalLength As Integer = 6 '24
+    '                    '    pts.Add(New PointF((Me.ClientRectangle.Width / 2) + offsetX, (Me.ClientRectangle.Height / 2) - verticalLength + offsetY))
+    '                    '    pts.Add(New PointF((Me.ClientRectangle.Width / 2) + offsetX, (Me.ClientRectangle.Height / 2) + verticalLength + offsetY)) 'Me.LineShape1.EndPoint = 
+
+    '                    '    '  HORIZONTAL CROSSHAIR LINE
+    '                    '    pts.Add(New PointF((Me.ClientRectangle.Width / 2) - verticalLength + offsetX, (Me.ClientRectangle.Height / 2) + offsetY))
+    '                    '    pts.Add(New PointF((Me.ClientRectangle.Width / 2) + verticalLength + offsetX, (Me.ClientRectangle.Height / 2) + offsetY))
+    '                    '    eclipsePen = New Pen(eclipseBrush)
+    '                    '    eclipsePen.Width = lineWidth
+    '                    '    g.DrawLine(eclipsePen, pts(0), pts(1))
+    '                    '    g.DrawLine(eclipsePen, pts(2), pts(3))
+    '                    'Catch ex As Exception
+    '                    '    Err.Clear()
+    '                    'End Try
+    '                    'Application.DoEvents()
+    '                Catch ex As Exception
+    '                    Err.Clear()
+    '                Finally
+    '                    g.Dispose()
+    '                End Try
+    '            End Using
+    '        End If
+    '        'timeOutTime = DateTime.Now
+    '        'End If
+    '        Return
+    '    Catch ex As Exception
+    '        Err.Clear()
+    '    Finally
+    '        Me.TopMost = True
+    '        Me.BringToFront()
+    '        Me.Invalidate(true)
+    '    End Try
+    'End Sub
+
+    'Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+    '    Try
+    '        ' NUM-KEY LOCK TOGGGLES CROSS HAIR DOT
+    '        'If True = True Then 'If timeOutTime.AddMilliseconds(timeout) <= DateTime.Now Then '
+    '        If pauseDrawing = False Or Control.IsKeyLocked(Keys.NumLock) = True Then
+    '            baseColor1 = Color.FromArgb(eclipseBrushTransparency, baseColor1)
+    '            Using g As Graphics = Graphics.FromHwnd(IntPtr.Zero)
+    '                Try
+    '                    ' SET IMAGE SETTINGS
+    '                    g.CompositingQuality = Drawing2D.CompositingQuality.AssumeLinear
+    '                    g.InterpolationMode = Drawing2D.InterpolationMode.HighQualityBicubic
+    '                    g.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+    '                    g.PixelOffsetMode = PixelOffsetMode.HighQuality
+    '                    g.CompositingMode = CompositingMode.SourceOver
+
+    '                    ' GET CENTER PIXEL COLOR ON SCREEN
+    '                    Dim Bmp As New Bitmap(1, 1)
+    '                    Dim gTemp As Graphics = Graphics.FromImage(Bmp)
+    '                    gTemp.CopyFromScreen(New Point(CInt((Me.ClientRectangle.Width / 2) + offsetX - eclipseDiameter / 2), CInt((Me.ClientRectangle.Height / 2) + offsetY - eclipseDiameter / 2)), New Point(0, 0), Bmp.Size)
+    '                    Dim colorOfScreenPixel As Color = Color.FromArgb(255, Bmp.GetPixel(0, 0))
+
+    '                    'Dim eclipseBrushColor As Color = baseColor1
+    '                    ' DRAW SOLID CIRCLE IN CENTER OF SCREEN
+    '                    'eclipseBrush = New SolidBrush(Color.FromArgb(CInt(255), Color.White))
+    '                    'Dim eclipseDiameter2 As Single = eclipseDiameter * 2
+    '                    'g.FillEllipse(eclipseBrush, CSng((Me.ClientRectangle.Width / 2) + offsetX - eclipseDiameter2 / 2), CSng((Me.ClientRectangle.Height / 2) + offsetY - eclipseDiameter2 / 2), eclipseDiameter2, eclipseDiameter2)
+
+    '                    'eclipseBrushColor = baseColor1
+    '                    ' DRAW SOLID CIRCLE IN CENTER OF SCREEN
+
+    '                    eclipseBrush = New SolidBrush(baseColor1)
+    '                    g.FillEllipse(eclipseBrush, CSng((Me.ClientRectangle.Width / 2) + offsetX - eclipseDiameter / 2), CSng((Me.ClientRectangle.Height / 2) + offsetY - eclipseDiameter / 2), eclipseDiameter, eclipseDiameter)
+
+    '                    eclipsePen.Width = 1.0F
+    '                    g.DrawEllipse(eclipsePen, CSng((Me.ClientRectangle.Width / 2) + offsetX - eclipseDiameter / 2), CSng((Me.ClientRectangle.Height / 2) + offsetY - eclipseDiameter / 2), eclipseDiameter, eclipseDiameter)
+
+    '                    'OUTER ECLIPSE
+    '                    'Try
+    '                    '    'CROSSHAIRS
+    '                    '    Dim pts As New List(Of PointF)
+    '                    '    '  VERTICAL CROSSHAIR LINE
+    '                    '    Dim verticalLength As Integer = 6 '24
+    '                    '    pts.Add(New PointF((Me.ClientRectangle.Width / 2) + offsetX, (Me.ClientRectangle.Height / 2) - verticalLength + offsetY))
+    '                    '    pts.Add(New PointF((Me.ClientRectangle.Width / 2) + offsetX, (Me.ClientRectangle.Height / 2) + verticalLength + offsetY)) 'Me.LineShape1.EndPoint = 
+
+    '                    '    '  HORIZONTAL CROSSHAIR LINE
+    '                    '    pts.Add(New PointF((Me.ClientRectangle.Width / 2) - verticalLength + offsetX, (Me.ClientRectangle.Height / 2) + offsetY))
+    '                    '    pts.Add(New PointF((Me.ClientRectangle.Width / 2) + verticalLength + offsetX, (Me.ClientRectangle.Height / 2) + offsetY))
+    '                    '    eclipsePen = New Pen(eclipseBrush)
+    '                    '    eclipsePen.Width = lineWidth
+    '                    '    g.DrawLine(eclipsePen, pts(0), pts(1))
+    '                    '    g.DrawLine(eclipsePen, pts(2), pts(3))
+    '                    'Catch ex As Exception
+    '                    '    Err.Clear()
+    '                    'End Try
+    '                    'Application.DoEvents()
+    '                Catch ex As Exception
+    '                    Err.Clear()
+    '                Finally
+    '                    g.Dispose()
+    '                End Try
+    '            End Using
+    '        End If
+    '        'timeOutTime = DateTime.Now
+    '        'End If
+    '        Return
+    '    Catch ex As Exception
+    '        Err.Clear()
+    '    Finally
+    '        Me.TopMost = True
+    '        Me.BringToFront()
+    '        Me.Invalidate(true)
+    '    End Try
+    'End Sub
+
     Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
         pauseDrawing = True
-        Me.Invalidate()
+        Me.Invalidate(True)
     End Sub
+
+    'Private Sub Timer2_Tick(sender As Object, e As EventArgs)
+    '    Try
+    '        ' NUM-KEY LOCK TOGGGLES CROSS HAIR DOT
+    '        'If True = True Then 'If timeOutTime.AddMilliseconds(timeout) <= DateTime.Now Then '
+    '        If pauseDrawing = False Or Control.IsKeyLocked(Keys.NumLock) = True Then
+    '            baseColor1 = Color.FromArgb(eclipseBrushTransparency, baseColor1)
+    '            Using g As Graphics = Graphics.FromHwnd(IntPtr.Zero)
+    '                Try
+    '                    ' SET IMAGE SETTINGS
+    '                    g.CompositingQuality = Drawing2D.CompositingQuality.AssumeLinear
+    '                    g.InterpolationMode = Drawing2D.InterpolationMode.HighQualityBicubic
+    '                    g.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+    '                    g.PixelOffsetMode = PixelOffsetMode.HighQuality
+    '                    g.CompositingMode = CompositingMode.SourceOver
+
+    '                    ' GET CENTER PIXEL COLOR ON SCREEN
+    '                    Dim Bmp As New Bitmap(1, 1)
+    '                    Dim gTemp As Graphics = Graphics.FromImage(Bmp)
+    '                    gTemp.CopyFromScreen(New Point(CInt((Me.ClientRectangle.Width / 2) + offsetX - eclipseDiameter / 2), CInt((Me.ClientRectangle.Height / 2) + offsetY - eclipseDiameter / 2)), New Point(0, 0), Bmp.Size)
+    '                    Dim colorOfScreenPixel As Color = Color.FromArgb(255, Bmp.GetPixel(0, 0))
+
+    '                    'Dim eclipseBrushColor As Color = baseColor1
+    '                    ' DRAW SOLID CIRCLE IN CENTER OF SCREEN
+    '                    'eclipseBrush = New SolidBrush(Color.FromArgb(CInt(255), Color.White))
+    '                    'Dim eclipseDiameter2 As Single = eclipseDiameter * 2
+    '                    'g.FillEllipse(eclipseBrush, CSng((Me.ClientRectangle.Width / 2) + offsetX - eclipseDiameter2 / 2), CSng((Me.ClientRectangle.Height / 2) + offsetY - eclipseDiameter2 / 2), eclipseDiameter2, eclipseDiameter2)
+
+    '                    'eclipseBrushColor = baseColor1
+    '                    ' DRAW SOLID CIRCLE IN CENTER OF SCREEN
+
+    '                    eclipseBrush = New SolidBrush(baseColor1)
+    '                    g.FillEllipse(eclipseBrush, CSng((Me.ClientRectangle.Width / 2) + offsetX - eclipseDiameter / 2), CSng((Me.ClientRectangle.Height / 2) + offsetY - eclipseDiameter / 2), eclipseDiameter, eclipseDiameter)
+
+    '                    eclipsePen.Width = 1.0F
+    '                    g.DrawEllipse(eclipsePen, CSng((Me.ClientRectangle.Width / 2) + offsetX - eclipseDiameter / 2), CSng((Me.ClientRectangle.Height / 2) + offsetY - eclipseDiameter / 2), eclipseDiameter, eclipseDiameter)
+
+    '                    'OUTER ECLIPSE
+    '                    'Try
+    '                    '    'CROSSHAIRS
+    '                    '    Dim pts As New List(Of PointF)
+    '                    '    '  VERTICAL CROSSHAIR LINE
+    '                    '    Dim verticalLength As Integer = 6 '24
+    '                    '    pts.Add(New PointF((Me.ClientRectangle.Width / 2) + offsetX, (Me.ClientRectangle.Height / 2) - verticalLength + offsetY))
+    '                    '    pts.Add(New PointF((Me.ClientRectangle.Width / 2) + offsetX, (Me.ClientRectangle.Height / 2) + verticalLength + offsetY)) 'Me.LineShape1.EndPoint = 
+
+    '                    '    '  HORIZONTAL CROSSHAIR LINE
+    '                    '    pts.Add(New PointF((Me.ClientRectangle.Width / 2) - verticalLength + offsetX, (Me.ClientRectangle.Height / 2) + offsetY))
+    '                    '    pts.Add(New PointF((Me.ClientRectangle.Width / 2) + verticalLength + offsetX, (Me.ClientRectangle.Height / 2) + offsetY))
+    '                    '    eclipsePen = New Pen(eclipseBrush)
+    '                    '    eclipsePen.Width = lineWidth
+    '                    '    g.DrawLine(eclipsePen, pts(0), pts(1))
+    '                    '    g.DrawLine(eclipsePen, pts(2), pts(3))
+    '                    'Catch ex As Exception
+    '                    '    Err.Clear()
+    '                    'End Try
+    '                    'Application.DoEvents()
+    '                Catch ex As Exception
+    '                    Err.Clear()
+    '                Finally
+    '                    g.Dispose()
+    '                End Try
+    '            End Using
+    '        End If
+    '        'timeOutTime = DateTime.Now
+    '        'End If
+    '        Return
+    '    Catch ex As Exception
+    '        Err.Clear()
+    '    Finally
+    '        Me.TopMost = True
+    '        Me.BringToFront()
+    '        Me.Invalidate(true)
+    '    End Try
+    'End Sub
 End Class
